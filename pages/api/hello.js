6f253b335e5491d9f2b2e8515b830b56c4f8625d
Lia <lia1.perochaud@epitech.eu>
2021-07-08 00:36:42 +0200

    init db in sql

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const response = await prisma.events.findUnique({
+    where: { id: 1 },
+  });
+  const players = await prisma.players.findMany({
+    where: { event_id: 1 },
+  });
+  const scores = await prisma.scores.findMany({
+    where: { event_id: 1 },
+    orderBy: { score: "desc" },
+  });
+  console.log(players);
+  console.log(scores);
+  var test = 1;
+  for (const score of scores) {
+    for (const player of players) {
+      if (score.player_id === player.id) {
+        console.log(test, ":", player.name, "with", score.score);
+        test++;
+      }
+    }
+  }
+  console.log(response);
+  res.status(200).json(response);
+};
