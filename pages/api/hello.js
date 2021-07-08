620178ec65b83649b8ac1504f1de43e1cd2e0f5c
Lia <lia1.perochaud@epitech.eu>
2021-07-08 02:40:31 +0200

    link db and frontend

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,27 +3,40 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async (req, res) => {
-  const response = await prisma.events.findUnique({
-    where: { id: 1 },
+  const response = await prisma.events.findMany({
+    orderBy: { id: "desc" },
   });
   const players = await prisma.players.findMany({
-    where: { event_id: 1 },
+    where: { event_id: response[0].id },
   });
   const scores = await prisma.scores.findMany({
-    where: { event_id: 1 },
+    where: { event_id: response[0].id },
     orderBy: { score: "desc" },
   });
-  console.log(players);
-  console.log(scores);
-  var test = 1;
+
+  var test = 1,
+    first = 1,
+    second = 1,
+    third = 1;
   for (const score of scores) {
     for (const player of players) {
       if (score.player_id === player.id) {
-        console.log(test, ":", player.name, "with", score.score);
+        if (test === 1) {
+          first = player.name;
+        } else if (test === 2) {
+          second = player.name;
+        } else if (test === 3) {
+          third = player.name;
+        }
         test++;
       }
     }
   }
-  console.log(response);
-  res.status(200).json(response);
+  res.status(200).json({
+    first: first,
+    second: second,
+    third: third,
+    eventName: response[0].name,
+    eventDate: response[0].start_date,
+  });
 };
